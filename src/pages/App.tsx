5e647a905f7903c19331a2fc41a6abd2a29286f8
mckoyd <dominick.mckoy@gmail.com>
2024-03-24 01:15:58 -0700

    adds folder structure

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import './App.css';
-import logo from './logo.svg';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
 
 function App() {
   return (
