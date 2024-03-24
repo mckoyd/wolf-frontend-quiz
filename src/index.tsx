e941e9adef248d4a7b97245c75f0a35814af8f25
mckoyd <dominick.mckoy@gmail.com>
2024-03-24 00:35:23 -0700

    initial commit | sets up linter, prettier, and husky

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const test = 'Testing linter';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
