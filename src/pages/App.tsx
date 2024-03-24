b36562d1e3a18ca99a44b4c2a6c8837b52d94350
mckoyd <dominick.mckoy@gmail.com>
2024-03-24 03:11:41 -0700

    moves types to dev deps

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import logo from '../assets/logo.svg';
 import '../styles/App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-section">
       <Routes>
         <Route path="/" element={<h1>From Root</h1>} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
